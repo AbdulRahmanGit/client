ff9c199134bf1a21ef08bfe209bdc8bf5a93ab78
Abdul Rahman <102407007+AbdulRahmanGit@users.noreply.github.com>
2024-02-11 16:00:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -142,7 +142,7 @@ const handleKeyDown = (e) => {
   )}
              
             </div>
-            <p className= "info"> ChatGPT can make mistakes. Consider checking important information.</p>
+            <p className= "info"> ChatGPT can make mistakes. Consider checking important information. Feedback will be Appreciated</p>
         </div>
     </section>
       
